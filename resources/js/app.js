6a113dcc55f0a21f668d8d062a61c4641261faed
Thifany Nicastro <thifanynicastro@hotmail.com>
2020-11-13 22:33:25 -0300

    Install bootstrap, jquery, popper, font awesome

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-require('./bootstrap');
+// window.Popper = require('popper.js').default;
+window.$ = window.jQuery = require('jquery');
+// window.Popper = require('popper.js').default;
+require('bootstrap');
+
+// require('jquery');
+
+// require('popper.js');
+
+// window.Jquery = require('jquery');
+
+// import $ from 'jquery';
+// window.$ = window.jQuery = $;
+
+// window.$ = window.jQuery = require('jquery');
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- CSRF Token -->
     {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
@@ -24,7 +24,8 @@
     <!-- Bootstrap -->
     {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> --}}
 
-    <script src="https://kit.fontawesome.com/596c2e9bb6.js" crossorigin="anonymous"></script>
+    <!-- Font Awesome -->
+    {{-- <script src="https://kit.fontawesome.com/596c2e9bb6.js" crossorigin="anonymous"></script> --}}
 
     @notifyCss
 </head>
@@ -107,17 +108,30 @@
 
     <main class="py-4">
         @yield('content')
+
+        <button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
     </main>
 
     @include('notify::messages')
 
+    
+
     {{-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+    </script> --}}
+    {{-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> --}}
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
     </script> --}}
+
+    <script src="{{ asset('js/app.js') }}"></script>
     @notifyJs
+    <script>
+        $(function () {
+            $('[data-toggle="popover"]').popover()
+        })
+    </script>
+    
 </body>
 
 </html>
